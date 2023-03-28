df17876bfc0771d3e0a5df4261cde532d6bc0de2
tommy <tommy@kali-tommy.tommy>
2023-03-28 21:18:33 +0200

    filter urls

--- Aracnida/main.py
+++ Aracnida/main.py
@@ -2,13 +2,15 @@ import requests
 import re
 
 def get_one_url():
-    url = 'https://www.cesif.es'
+    url = 'https://www.cesif.es/'
     r = requests.get('https://www.cesif.es')
     urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+[/\w\.-]*(?:\?[\w\d%&=]*)?(?:#[\w\d-]*)?', r.text)
+    urls = set(urls)
+    urls = list(urls)
     for e in urls:
         if url not in e:
             urls.remove(e)
-    print(urls)
+            continue
 
 if __name__ == '__main__':
     get_one_url()
