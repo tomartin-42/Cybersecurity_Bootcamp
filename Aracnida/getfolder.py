f0ecc4e71f3329e2186738cae2c775e18a8271dd
tommy <tommy@kali-tommy.tommy>
2023-03-30 11:33:19 +0200

    aracnida adjust regex

--- Aracnida/getfolder.py
+++ Aracnida/getfolder.py
@@ -1,12 +1,12 @@
 import requests
 
 class GetTargets:
-    def __init__(url, deep_lvl, targets = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']):
+    def __init__(self, url, deep_lvl, targets = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']):
         self.url = url
         self.deep_lvl = deep_lvl
         self.target = targets
 
-    def __get_text_url(url):
-        self.r = request.get(url)
+    def __get_text_url(self, url):
+        self.r = requests.get(url)
 
 
--- Aracnida/main.py
+++ Aracnida/main.py
@@ -4,13 +4,14 @@ import re
 def get_one_url():
     url = 'https://www.cesif.es/'
     r = requests.get('https://www.cesif.es')
-    urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+[/\w\.-]*(?:\?[\w\d%&=]*)?(?:#[\w\d-]*)?', r.text)
+    urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+[/\w\.-]*(?:\?[\w\d%&=]*)?(?:#[\w\d-]*)?(?<![\.,])', r.text)
     urls = set(urls)
     urls = list(urls)
-    for e in urls:
+    for e in urls: 
         if url not in e:
             urls.remove(e)
             continue
+    print(urls)
 
 if __name__ == '__main__':
     get_one_url()
