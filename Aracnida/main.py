da88cc56dd594abfb7e191a058a4d3b2c9bd40e3
tommy <tommy@kali-tommy.tommy>
2023-04-01 00:30:13 +0200

    refactory

--- Aracnida/main.py
+++ Aracnida/main.py
@@ -1,26 +1,35 @@
 import requests
 import re
 
-def get_urls(url, urls):
-    dic_urls = {url:1}
+
+def unmount_url(url, lvl):
+    pass
+
+
+def get_urls(url, urls, lvl=1):
+    dic_urls = {url: lvl}
     for e in urls:
         if '/' in e[len(url):]:
-            dic_urls[e[len(url):].split('/')[1]] = 2
-    print(dic_urls)
+            dic_urls[url + e[len(url):].split('/')[1]] = 2
+  #  print(dic_urls)
 
 
 def get_one_url(url):
     r = requests.get('https://www.cesif.es')
-    urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+[/\w\.-]*(?:\?[\w\d%&=]*)?(?:#[\w\d-]*)?(?<![\.,])', r.text)
+    regex = re.compile(
+        r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+[/\w\.-]*(?:\?[\w\d%&=]*)?(?:#[\w\d-]*)?(?<![\.,])')
+    urls = re.findall(regex, r.text)
     urls = set(urls)
     urls = list(urls)
-    for e in urls: 
+    for e in urls:
         if url != e[0:len(url)]:
             urls.remove(e)
+            continue
+        print(e)
     return urls
+
 
 if __name__ == '__main__':
     x = get_one_url('https://www.cesif.es/')
-    print(x)
-    get_urls('https://www.cesif.es/', x) 
-
+#    print(x)
+    get_urls('https://www.cesif.es/', x)
