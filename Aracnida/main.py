6346708b3babc15054d647243c313e68a250d7b3
tommy <tommy@kali-tommy.tommy>
2023-03-31 11:08:51 +0200

    init split urls

--- Aracnida/main.py
+++ Aracnida/main.py
@@ -1,8 +1,15 @@
 import requests
 import re
 
-def get_one_url():
-    url = 'https://www.cesif.es/'
+def get_urls(url, urls):
+    dic_urls = {url:1}
+    for e in urls:
+        if '/' in e[len(url):]:
+            dic_urls[e[len(url):].split('/')[1]] = 2
+    print(dic_urls)
+
+
+def get_one_url(url):
     r = requests.get('https://www.cesif.es')
     urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+[/\w\.-]*(?:\?[\w\d%&=]*)?(?:#[\w\d-]*)?(?<![\.,])', r.text)
     urls = set(urls)
@@ -10,7 +17,10 @@ def get_one_url():
     for e in urls: 
         if url != e[0:len(url)]:
             urls.remove(e)
-            continue
+    return urls
 
 if __name__ == '__main__':
-    get_one_url()
+    x = get_one_url('https://www.cesif.es/')
+    print(x)
+    get_urls('https://www.cesif.es/', x) 
+
