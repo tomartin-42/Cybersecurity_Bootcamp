7901452be0d0ad4950d666f1d0ba46e798efee96
Tomas alejandro Martinez cruz <tomartin@c3r7s4.42madrid.com>
2023-04-02 19:58:07 +0200

    generate sub url dict

--- Aracnida/main.py
+++ Aracnida/main.py
@@ -1,35 +1,48 @@
 import requests
 import re
 
+file_include = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.docx', '.pdf']
+file_list = []
+#urls_list = {'https://www.cesif.es': 1}
+urls_list = {}
+max_lvl = 7
 
-def unmount_url(url, lvl):
-    pass
+def recursive_get_partials_url(main_url, complete_url, lvl):
+    if lvl >= max_lvl or main_url[:-1] == complete_url:
+        return
+    else:
+        aux = complete_url[len(main_url):]
+        add_str = aux.split('/')[0]
+        urls_list[main_url + add_str] = lvl + 1
+        recursive_get_partials_url(main_url + add_str + '/', complete_url, lvl + 1)
 
 
-def get_urls(url, urls, lvl=1):
-    dic_urls = {url: lvl}
-    for e in urls:
-        if '/' in e[len(url):]:
-            dic_urls[url + e[len(url):].split('/')[1]] = 2
-  #  print(dic_urls)
-
-
-def get_one_url(url):
+def get_one_url(url, lvl=0):
     r = requests.get('https://www.cesif.es')
-    regex = re.compile(
-        r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+[/\w\.-]*(?:\?[\w\d%&=]*)?(?:#[\w\d-]*)?(?<![\.,])')
-    urls = re.findall(regex, r.text)
+    urls = re.findall(
+        'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+[/\w\.-]*(?:\?[\w\d%&=]*)?(?:#[\w\d-]*)?(?<![\.,])', r.text)
     urls = set(urls)
     urls = list(urls)
     for e in urls:
         if url != e[0:len(url)]:
             urls.remove(e)
-            continue
-        print(e)
+            print(e)
+    print("DEL1------------------")
+    for e in urls:
+        if e.find(url) == -1:
+            urls.remove(e)
+            print(e)
+    print("DEL2------------------")
+    for e in urls:
+        recursive_get_partials_url(url, e, lvl)
     return urls
 
 
 if __name__ == '__main__':
-    x = get_one_url('https://www.cesif.es/')
-#    print(x)
-    get_urls('https://www.cesif.es/', x)
+    x = get_one_url('https://www.cesif.es')
+    #recursive_get_partials_url('https://www.cesif.es', 'https://www.cesif.es/uno/dos/tres/cuatro/cinco', 1)
+    #recursive_get_partials_url('https://www.cesif.es', 'https://www.cesif.es/uno/dos/tres', 1)
+    #recursive_get_partials_url('https://www.cesif.es', 'https://www.cesif.es/uno/dos/tres/kk.gif', 0)
+    #print(urls_list)
+    for e in urls_list:
+        print(e, urls_list[e])
