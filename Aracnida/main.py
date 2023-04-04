b99919b7ec3ab71d1155e9e89206f4edbb291731
tommy <tommy@kali-tommy.tommy>
2023-04-04 23:28:46 +0200

    incorrect logic

--- Aracnida/main.py
+++ Aracnida/main.py
@@ -4,8 +4,22 @@ import re
 file_include = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.docx', '.pdf']
 file_list = []
 # urls_list = {'https://www.cesif.es': 1}
-urls_list = {}
-max_lvl = 7
+max_lvl = 3
+url_to_visit = set()
+file_list = set()
+visit_list = set()
+
+
+def extract_urls(urls_list):
+    for e in urls_list:
+        url_to_visit.add(e)
+
+
+def extract_files(urls_list):
+    for e in urls_list.copy():
+        if re.search(r'\.[a-zA-Z0-9]{2,4}$', e):
+            file_list.add(e)
+            urls_list.pop(e)
 
 
 def delete_no_accept_files(url, urls_list):
@@ -20,7 +34,7 @@ def delete_no_accept_files(url, urls_list):
                 urls_list.pop(e)
 
 
-def recursive_get_partials_url(main_url, complete_url, lvl):
+def recursive_get_partials_url(main_url, complete_url, lvl, urls_list):
     if lvl >= max_lvl or main_url[:-1] == complete_url:
         return
     else:
@@ -28,32 +42,47 @@ def recursive_get_partials_url(main_url, complete_url, lvl):
         add_str = aux.split('/')[0]
         urls_list[main_url + add_str] = lvl + 1
         recursive_get_partials_url(main_url + add_str + '/',
-                                   complete_url, lvl + 1)
+                                   complete_url, lvl + 1, urls_list)
 
 
-def get_one_url(url, lvl=0):
-    r = requests.get('https://www.cesif.es')
-    urls = re.findall(
-        'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+[/\w\.-]*(?:\?[\w\d%&=]*)?(?:#[\w\d-]*)?(?<![\.,])', r.text)
-    urls = set(urls)
-    urls = list(urls)
+def clean_up_urls(url, urls):
     for e in urls:
         if url != e[0:len(url)]:
             urls.remove(e)
     for e in urls:
         if e.find(url) == -1:
             urls.remove(e)
-    for e in urls:
-        recursive_get_partials_url(url, e, lvl)
-    delete_no_accept_files(url, urls_list)
-    return urls
+
+
+def get_one_url(url, lvl=0):
+    urls_list = {}
+    if url not in visit_list:
+        visit_list.add(url)
+        r = requests.get(url)
+        urls = re.findall(
+            'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+[/\w\.-]*(?:\?[\w\d%&=]*)?(?:#[\w\d-]*)?(?<![\.,])', r.text)
+        urls = set(urls)
+        urls = list(urls)
+        clean_up_urls(url, urls)
+        for e in urls:
+            recursive_get_partials_url(url, e, lvl, urls_list)
+        delete_no_accept_files(url, urls_list)
+        extract_files(urls_list)
+        extract_urls(urls_list)
 
 
 if __name__ == '__main__':
-    x = get_one_url('https://www.cesif.es')
+    i = 0
+    get_one_url('https://www.cesif.es', i)
+    # for e in urls_list.copy():
+    #    get_one_url(e, urls_list[e])
     # recursive_get_partials_url('https://www.cesif.es', 'https://www.cesif.es/uno/dos/tres/cuatro/cinco', 1)
     # recursive_get_partials_url('https://www.cesif.es', 'https://www.cesif.es/uno/dos/tres', 1)
     # recursive_get_partials_url('https://www.cesif.es', 'https://www.cesif.es/uno/dos/tres/kk.gif', 0)
     # print(urls_list)
-    for e in urls_list:
-        print(e, urls_list[e])
+    i += 1
+    for e in url_to_visit.copy():
+        get_one_url(e, i)
+        i += 1
+    print(file_list)
+    # print(url_to_visit)
