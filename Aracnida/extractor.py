b275c8b5775d027dbf454191bfed9af97fca7e19
Tomas alejandro Martinez cruz <tomartin@c2r2s6.42madrid.com>
2023-04-06 16:54:56 +0200

    mierda

--- Aracnida/extractor.py
+++ Aracnida/extractor.py
@@ -1,8 +1,10 @@
 import requests
 import re
+from pwn import *
+import time
 
 
-class GetTargets:
+class Extractor:
     file_include = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.docx', '.pdf']
 
     def __init__(self, url, deep_lvl=5):
@@ -15,16 +17,24 @@ class GetTargets:
         file_list = self._extract_files(temp, self.url)
         self.url_to_visit = self._extract_urls(temp)
         self.url_to_visit.add(self.url)
+        p1 = log.progress('Spider') 
         while True:
             for e in self.url_to_visit:
                 if e not in self.visit_list:
+                    p1.status('Scaning URL: ' + e + '')
                     temp = self._get_one_url(e)
-                    file_list.difference(self._extract_files(
-                        temp, 'https://www.cesif.es'))
                     tmp_visit = self._extract_urls(temp)
+                    file_tmp = self._extract_files(temp, self.url)
+                    self.file_list.update(file_tmp)
+                    #self.file_list.update(self._extract_files(
+                    #    temp, self.url))
+                    # print("filelist ", self.file_list)
                     break
-            self.url_to_visit.difference(tmp_visit)
-            if self.visit_list == self.url_to_visit:
+            try:
+                self.url_to_visit.update(tmp_visit)
+                if self.visit_list == self.url_to_visit:
+                    break
+            except:
                 break
 
     def _get_one_url(self, url, lvl=0):
@@ -51,14 +61,17 @@ class GetTargets:
                 urls.remove(e)
 
     def _recursive_get_partials_url(self, main_url, complete_url, lvl, urls_list):
-        if lvl >= self.max_lvl or main_url[:-1] == complete_url:
+        if lvl >= self.max_lvl or main_url == complete_url:
             return
         else:
+            slash = '/'
             aux = complete_url[len(main_url):]
-            add_str = aux.split('/')[0]
+            if aux.find('/') != -1:
+                add_str = aux[aux.find('/') + 1:]
+            else:
+                add_str = ''
             urls_list[main_url + add_str] = lvl + 1
-            self._recursive_get_partials_url(main_url + add_str + '/',
-                                             complete_url, lvl + 1, urls_list)
+            self._recursive_get_partials_url(main_url + add_str, complete_url, lvl + 1, urls_list)
 
     def _delete_no_accept_files(self, url, urls_list):
         for e in urls_list.copy():
@@ -80,10 +93,17 @@ class GetTargets:
     def _extract_files(self, urls_list, url):
         aux = set()
         for e in urls_list.copy():
-            if re.search(r'\.[a-zA-Z0-9]{2,4}$', e) and e != url:
+            if re.search(r'\.[a-zA-Z0-9]{2,4}$', e):
+                #print('scan file ', e)
                 urls_list.pop(e)
                 aux.add(e)
         return aux
 
     def get_list_file(self):
         return self.file_list
+
+    def get_visit_list(self):
+        return self.visit_list
+
+    def get_url_to_visit(self):
+        return self.url_to_visit
--- Aracnida/spider.py
+++ Aracnida/spider.py
@@ -1,4 +1,4 @@
-import getfolder
+import extractor
 import argparse
 
 description = 'Extract jpg, jpeg, png, gif, bmp, docx and pdf files from a url'
@@ -10,14 +10,20 @@ parser.add_argument('-l', type=int, default=99, nargs='?',
                     help='Inser deep lvl. If exclude deep lvl, spider get all files')
 parser.add_argument('-p', metavar='PATH', type=str, default='./data/',
                     help='Indicate path where download files')
-
 args = parser.parse_args()
 
 def spider(args):
     if args.l == None:
         args.l = 5
-    print("arg.r ", args.r, "\narg.l ", args.l, "\narg.p ", args.p)
-    pass
+    #print("arg.l ", args.l, "\narg.r ", args.r, "\narg.p ", args.p)
+    target = extractor.Extractor(args.r, args.l)
+    print("We have found ", len(target.get_list_file()), " files")
+    print("visit list ", target.get_visit_list())
+    print()
+    print("url to visit ", target.get_url_to_visit())
+    print()
+    print(target.get_list_file())
+    
 
 if __name__ == '__main__':
     spider(args)
