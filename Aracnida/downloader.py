34875d3ca59db7dc99cdccd84e20a3b4352c845f
tommy <tommy@kali-tommy.tommy>
2023-04-08 11:35:53 +0200

    downloader end

--- Aracnida/downloader.py
+++ Aracnida/downloader.py
@@ -2,12 +2,14 @@ import requests
 from multiprocessing.pool import ThreadPool
 from pwn import *
 import os
+from tqdm import tqdm
 
 class Downloader:
 
     def __init__(self, file_list, path):
         self.path = os.getcwd() + path
         self.download_list = {f"{self.path}{i:04}_{self.extract_file_name(file)}": file for i, file in enumerate(file_list)}
+        os.makedirs(self.path, exist_ok=True)
         self.multi_download()
 
     def extract_file_name(self, url):
