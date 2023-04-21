57b0450e0aad72d826f9cf5f0befbc7a72d56130
tommy <tommy@kali-tommy.tommy>
2023-04-21 23:20:00 +0200

    add venv

--- Aracnida/extractor.py
+++ Aracnida/extractor.py
@@ -15,74 +15,32 @@ class Extractor:
         self.visit_list = set()
         self.url_to_visit.add(self.url)
         p1 = log.progress('Spider') 
-        flag = True
-        try:
-            r = requests.get(self.url)
-            r.raise_for_status()
-        except requests.exceptions.ConnectionError:
-            print(f'Error de conexión en la solicitud a {self.url}')
-            exit(1)
-        while flag:
-            prev = len(self.url_to_visit)
-            for e in self.url_to_visit.copy():
-                p1.status('Scaning URL: ' + e + '')
-                tmp = self._get_one_url(e)
-                if tmp != None:
-                    self.file_list.update(self._extract_files(tmp))
-                    for r in tmp:
-                        self._extract_urls(r, e)
-            post = len(self.url_to_visit)
-            if prev == post:
-                flag = False
-        for e in self.file_list.copy():
-            aux = os.path.splitext(e)[1]
-            if aux not in self.file_include:
-                self.file_list.remove(e)
+        self._extract_urls(self.url_to_visit)
         
-
-    def _get_one_url(self, url):
-        if url not in self.visit_list:
-            self.visit_list.add(url)
-            r = requests.get(url)
-            urls = re.findall(
-                'https?://(?:[-\w.@#%]|(?:%[\da-fA-F]{2}))+[/\w\.-]*(?:\?[\w\d%&=]*)?(?:#[\w\d-]*)?(?<![\.,@-])', r.text)
-            urls = set(urls)
-            self._clean_up_urls(urls)
-            for e in urls.copy(): # remove element if not in range
-                if not self.in_range(e):
-                    urls.remove(e)
-            return urls 
-
-    def _clean_up_urls(self, urls):
-        for e in urls.copy():
-            if e.find(self.url) == -1:
-                urls.remove(e)
-
-
-    def in_range(self, element):
-        aux = element[len(self.url):]
-        if aux.count('/') < self.max_lvl and aux.count('/') != -1:
-            return True
-        else:  # if not in range, delete
-            return False
-
-    def _extract_urls(self, element, head_str):
-        if element == head_str:
-            return 
+    def _extract_urls(self, list_to_scan):
+        if len(list_to_scan) == 0 or self.max_lvl <= 0:
+            return
         else:
-            aux = element[len(head_str):]
-            num = aux[1:].find('/')
-            if num != -1:
-                self.url_to_visit.add(element[:len(head_str) + len(aux[:num + 1])])
-                self._extract_urls(element, element[:len(head_str) + len(aux[:num + 1])])
+            for e in list_to_scan:
+                self.visit_list.add(e)
+                r = requests.get(e)
+                urls_in_text = re.findall(
+                    'https?://(?:[-\w.@#%]|(?:%[\da-fA-F]{2}))+[/\w\.-]*(?:\?[\w\d%&=]*)?(?:#[\w\d-]*)?(?<![\.,@-])', r.text)
+                self._extract_files(urls_in_text)
+                #self.url_to_visit.add(urls_in_text)
+            self.max_lvl -= 1
+            self._extract_urls(urls_in_text)
 
     def _extract_files(self, list_):
-        aux = set()
         for e in list_.copy():
-            tmp = e[len(self.url):]
-            if re.search(r'\.[a-zA-Z0-9]{2,4}$', tmp):
-                aux.add(e)
-        return aux
+            if e.find(self.url) == -1:
+                list_.remove(e)
+                continue
+            print(os.path.splitext(e)[1])
+            if os.path.splitext(e)[1] in self.file_include:
+                print("Add to file_list: ", e)
+                self.file_list.add(e)
+                list_.remove(e)
 
     def get_list_file(self):
         for e in self.file_list.copy():
